30a92df533fceb8379be97f11a151b6be73d205f
Shubham <saurabharyan30@gmail.com>
2021-10-31 16:16:29 +0530

    hello

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Peer from "simple-peer";
 import io from "socket.io-client";
 import "./App.css";
 
-const socket = io.connect("https://webrtcprojectshubham.herokuapp.com");
+const socket = io.connect("https://sleepy-conscious-iguana.glitch.me");
 function App() {
   const [me, setMe] = useState("");
   const [stream, setStream] = useState();
